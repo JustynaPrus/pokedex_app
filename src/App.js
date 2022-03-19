30eed1f5ad70a57c70700f74ebb53177b7533cd5
Justyna Pruś <justyna.p.prus@gmail.com>
2022-03-19 12:56:55 +0100

    working API

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import DataComponent from "./DataComponent";
+
+function App() {
+  return (
+    <div>
+      <h1>Pokedex App</h1>
+      <DataComponent />
+    </div>
+  );
+}
+
+export default App;
